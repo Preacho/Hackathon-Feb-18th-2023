00a66d7b5bff6f6f3a3ad86599a37913adb2da5e
Huhoi <jsntruong03@gmail.com>
2023-02-18 14:17:20 -0800

    Word and Gameplay :)

--- app/src/main/java/com/example/lostintranslation/model/GamePlay.java
+++ app/src/main/java/com/example/lostintranslation/model/GamePlay.java
@@ -1,4 +1,31 @@
 package com.example.lostintranslation.model;
 
 public class GamePlay {
+
+    String selectedWord;
+    int difficulty;
+    int score;
+    int attempts;
+
+    public GamePlay(){
+        difficulty = 1;                 // Set to 1 by default
+        score = 0;                      // Set to 0 by default
+        attempts = difficulty*2 + 1;    // Set to 3 by default
+    }
+
+    public void addScore(){
+        score = 50 + difficulty*50;
+    }
+
+    public void reduceAttempts(){
+        attempts = attempts - 1;
+    }
+
+    public void setTranscribedWord(){
+        Word word = new Word();
+        selectedWord = word.transformed_word;
+    }
+
+
+
 }
