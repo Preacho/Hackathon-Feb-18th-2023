feade33982c255cdf12f25474db3b100656377ad
Preston <quachpreston@gmail.com>
2023-02-18 14:20:59 -0800

    Made Rules

--- app/src/main/java/com/example/lostintranslation/activity_play.java
+++ app/src/main/java/com/example/lostintranslation/activity_play.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class activity_play extends AppCompatActivity {
     ActionBar ab;
+    private long timeleftinmillis = 60000;
+    private CountDownTimer countDownTimer;
+    private boolean timerrunning;
+    private TextView timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -22,11 +29,45 @@ public class activity_play extends AppCompatActivity {
         Button key = findViewById(R.id.bt_viewKey);
         key.setOnClickListener(v-> openKey());
 
+        timer = (TextView) findViewById(R.id.TimeCounter);
+        Timer();
     }
 
     private void openKey() {
         Intent intent = new Intent(this, activity_view_key.class);
+        pausetimer();
         startActivity(intent);
+        resumetimer();
+
 
     }
+    private void Timer(){
+        countDownTimer = new CountDownTimer(timeleftinmillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeleftinmillis = millisUntilFinished;
+                updatetimer();
+
+            }
+
+
+            @Override
+            public void onFinish() {
+                finish();
+            }
+        }.start();
+        timerrunning = true;
+
+    }
+    private void updatetimer() {
+        int seconds = (int) timeleftinmillis/1000;
+        timer.setText(Integer.toString(seconds));
+    }
+    private void pausetimer(){
+        countDownTimer.cancel();
+    }
+    private void resumetimer(){
+        Timer();
+    }
+
 }
