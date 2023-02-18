c8e6f2c58eb72d527ef4cadf551d92a7796bb462
Fraser <fgw@sfu.ca>
2023-02-18 11:13:22 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/lostintranslation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lostintranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
