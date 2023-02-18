23ceece8f2348c27035ac615783f511fab466aed
Fraser <fgw@sfu.ca>
2023-02-18 11:39:52 -0800

    Initialized buttons

--- app/src/main/java/com/example/lostintranslation/MainActivity.java
+++ app/src/main/java/com/example/lostintranslation/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lostintranslation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button rules = findViewById(R.id.bt_rules);
+        Button settings = findViewById(R.id.bt_settings);
+        Button play = findViewById(R.id.bt_play);
+
+        rules.setOnClickListener(v-> openRules());
+    }
+
+    private void openRules() {
+        Intent intent = new Intent();
     }
 }
